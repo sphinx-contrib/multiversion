1bb3ff4343e95faa35a29573e329fa5709338ac4
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-03-11 13:50:05 +0100

    setup.py: Add more metadata

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os.path
 from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as f:
+    readme = f.read()
 
 setup(
     name="sphinx-multiversion",
     description="Add support for multiple versions to sphinx",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     author="Jan Holthuis",
     author_email="holthuis.jan@googlemail.com",
+    url="https://holzhaus.github.io/sphinx-multiversion/",
     version="1.0.0",
     install_requires=["sphinx >= 2.1"],
     license="BSD",
