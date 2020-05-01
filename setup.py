f94f7e56b391c736a89243fcc82e03b7d25fa43e
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-05-01 23:12:27 +0200

    Increase version to 0.2.2 and add changelog entry

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     author="Jan Holthuis",
     author_email="holthuis.jan@googlemail.com",
     url="https://holzhaus.github.io/sphinx-multiversion/",
-    version="0.2.1",
+    version="0.2.2",
     install_requires=["sphinx >= 2.1"],
     license="BSD",
     packages=["sphinx_multiversion"],
