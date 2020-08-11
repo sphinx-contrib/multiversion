7109646da754d0eb29bbb9c9921b79af2c5b1259
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-08-11 23:21:21 +0200

    docs: Add alabaster theme options and GitHub/Travis links

--- docs/conf.py
+++ docs/conf.py
@@ -8,6 +8,15 @@ release = "0.2.4"
 version = "0.2"
 copyright = "{}, {}".format(time.strftime("%Y"), author)
 
+html_theme = "alabaster"
+html_theme_options = {
+    "github_repo": "sphinx-multiversion",
+    "github_user": "Holzhaus",
+    "github_banner": True,
+    "github_button": True,
+    "travis_button": True,
+    "show_relbar_bottom": True,
+}
 html_last_updated_fmt = "%c"
 master_doc = "index"
 pygments_style = "friendly"
